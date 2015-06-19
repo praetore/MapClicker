54718fe0b581423a88ed690dc95a5057aa1d5c39
darryl <d.amatsetam@gmail.com>
2015-06-19 15:59:57 +0200

    added display info, delete last record, csv export

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+import csv
+from tempfile import NamedTemporaryFile
+from flask import Flask, send_file
 from flask.ext.restless import APIManager
 from flask.ext.sqlalchemy import SQLAlchemy
 
@@ -11,17 +13,27 @@ class Point(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     lat = db.Column(db.Float, nullable=False)
     long = db.Column(db.Float, nullable=False)
-    schematic = db.Column(db.String)
+    schematic = db.Column(db.String, nullable=False)
 
 db.create_all()
 
 manager = APIManager(app, flask_sqlalchemy_db=db)
 
-manager.create_api(Point, methods=['GET', 'POST'])
+manager.create_api(Point, methods=['GET', 'POST', 'DELETE'])
 
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
 
+@app.route('/export')
+def export():
+    points = Point.query.all()
+    with NamedTemporaryFile('w+t', delete=False, suffix='.csv') as f:
+        wr = csv.writer(f, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+        [wr.writerow([getattr(record, column.name) for column in Point.__mapper__.columns]) for record in points]
+        fname = f.name
+    return send_file(fname, as_attachment=True)
+
 if __name__ == '__main__':
+    app.debug = True
     app.run()
