9ded3a3aaa4d1d5e5153cb1d950d5c79a02f9e20
darryl <d.amatsetam@gmail.com>
2015-08-22 21:24:17 +0200

    falling back to earlier server config

--- app.py
+++ app.py
@@ -116,3 +116,6 @@ def export():
          record in points]
         fname = f.name
     return send_file(fname, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run()
--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os
+import imp
 
 virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
 virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
@@ -8,12 +9,25 @@ try:
 except IOError:
     pass
 
+
+def run_gevent_server(app, ip, port=8080):
+    from gevent.pywsgi import WSGIServer
+    WSGIServer((ip, port), app).serve_forever()
+
+
+def run_simple_httpd_server(app, ip, port=8080):
+    from wsgiref.simple_server import make_server
+    make_server(ip, port, app).serve_forever()
+
+
 from app import app as application
 
 if __name__ == '__main__':
-    from wsgiref.simple_server import make_server
-    host = os.environ.get('OPENSHIFT_PYTHON_IP', 'localhost')
-    port = os.environ.get('OPENSHIFT_PYTHON_PORT', 8080)
-    httpd = make_server(host, port, application)
-    # Wait for a single request, serve it and quit.
-    httpd.handle_request()
+    ip = os.environ.get('OPENSHIFT_PYTHON_IP')
+    port = os.environ.get('OPENSHIFT_PYTHON_PORT')
+    zapp = imp.load_source('application', 'wsgi/application')
+
+    try:
+        run_gevent_server(application, ip, port)
+    except:
+        run_simple_httpd_server(zapp.application, ip, port)
