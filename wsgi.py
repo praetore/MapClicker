bd09baf17de767606c49d2150b97691e965e7f73
darryl <d.amatsetam@gmail.com>
2015-08-22 21:14:55 +0200

    changed openshift server config settings again sigh

--- wsgi.py
+++ wsgi.py
@@ -12,6 +12,8 @@ from app import app as application
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
-    httpd = make_server('localhost', 8051, application)
+    host = os.environ.get('OPENSHIFT_PYTHON_IP', 'localhost')
+    port = os.environ.get('OPENSHIFT_PYTHON_PORT', 8080)
+    httpd = make_server(host, port, application)
     # Wait for a single request, serve it and quit.
     httpd.handle_request()
